8cfa2b29bfb54096cf9aa4b219e08f46ebbd16df
Arumugam Veeman <arumugam.veeman@gmail.com>
2023-05-07 00:19:35 +0200

    second

---
+++ instance.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "ec2_instance" {
+    ami = "${var.ami_id}"
+    count = "${var.number_of_instances}"
+    subnet_id = "${var.subnet_id}"
+    instance_type = "${var.instance_type}"
+    key_name = "${var.ami_key_pair_name}"
+} 
