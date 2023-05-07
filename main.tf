c5a2cff6c6437c7f377a3803500edf852f8eefe5
Arumugam Veeman <arumugam.veeman@gmail.com>
2023-05-07 15:52:21 +0200

    test

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 provider "aws" {
-  access_key = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   region     = "eu-north-1"
 }
